5a68aacd6105ad99e0bb60a6a36302599bea7c6c
MabiGermano <mabi.germano.e@gmail.com>
2021-05-28 17:45:36 -0300

    settings to db connection + creating and listing rooms

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,31 @@
-import {Router} from 'express'
+import { json, Request, Response, Router } from 'express'
+import { getRepository } from 'typeorm'
+import { Room } from './models/Room'
 
 const routes = Router()
 
-routes.get('/', (req, res) => {
+routes.get('/', (req:Request, res:Response) => {
     return res.json({ message: 'Oiee' })
 })
 
+routes.get('/room', async (req:Request, res:Response) => {
+    const roomRepository = getRepository(Room)
+
+    const list = await roomRepository.find()
+    return res.json(list)
+})
+
+
+routes.post('/room', async (req:Request, res:Response) => {
+    const roomRepository = getRepository(Room)
+    console.log(req);
+    
+    const { identifier } = req.body
+    const room = { identifier }
+    const value = await roomRepository.save(room).catch((err) => {
+        console.log("Error: ", err);
+      })
+    return res.status(201).json(value)
+})
 
 export default routes
